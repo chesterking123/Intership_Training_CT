7bf12e5a9ef866f62758249e5e9c47cd205108d4
Deep Contractor <31130563+chesterking123@users.noreply.github.com>
2021-01-05 15:41:59 +0530

    Create PEP 8 - Naming Conventions.py

---
+++ PEP 8 - Naming Conventions.py
@@ -0,0 +1,37 @@
+# Naming Conventions
+
+my_module.py
+
+mypackage
+
+#function name
+def my_function():
+  
+  return
+
+#single letter variable
+x = 3
+
+#use underscore 
+my_variable = 'three' 
+
+#constants (caps)
+GRAVITY_ACC = 9.8
+
+#class name
+class ClassName():  
+  def marks():
+    pass
+
+#use apropriate variable name
+x = 'Deep Contractor' #wrong
+name = 'Deep Contractor' #correct
+
+#use apropriate function name
+def multiply_by_two():
+  return x*2
+  
+multiply_by_two(4)
+
+#conflicting_names_ (if name already exists ) 
+in_ = 'tango'
