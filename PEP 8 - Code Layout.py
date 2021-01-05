ecc78a83877df78cdd7a419887b683696b9fdb60
Deep Contractor <31130563+chesterking123@users.noreply.github.com>
2021-01-05 15:22:32 +0530

    Create PEP 8 - Code Layout.py

---
+++ PEP 8 - Code Layout.py
@@ -0,0 +1,41 @@
+#TWO BLANK LINE
+class FullTimeEmployee:
+  pass
+
+
+class PartTimeEmployee:
+  pass
+
+
+def Calculate():
+  pass
+ 
+ 
+def Print():
+  pass
+  
+  
+#SINGLE BLANK LINE
+class FullTimeEmployee:
+  def monthly_salary(self):
+    pass
+    
+  def yearly_salary(self):
+    pass
+   
+  def total_bonus(self):
+    pass
+
+#CLEAR STEPS
+def function(num):
+  a = 0
+  for i in range(10):
+    print(i)
+  print('Check')
+  
+  b=0
+  for j in range(20):
+    print(j)
+  print(check)
+  
+  return num
